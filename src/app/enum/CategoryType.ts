e67cc9b60d697c1137d3677374cbc3fe84378d1d
yacharenikrishnakanth <yacharenikrishnakanth@gmail.com>
2022-03-04 02:51:26 -0600

    Base setup

---
+++ src/app/enum/CategoryType.ts
@@ -0,0 +1,6 @@
+export enum CategoryType {
+  "Computers",
+  "Mobiles",
+  "TV & Home Theater",
+  "Video Games"
+}
---
+++ src/app/mockData.ts
@@ -0,0 +1,88 @@
+import {ProductInfo} from './models/productInfo';
+
+export const products: ProductInfo[] = [
+  {
+    productId: 'C0001',
+    productName: 'Apple Mac book Pro',
+    productPrice: 30.00,
+    productStock: 96,
+    productDescription: 'Books for learning Java',
+    productIcon: 'https://images-na.ssl-images-amazon.com/images/I/41f6Rd6ZEPL._SX363_BO1,204,203,200_.jpg',
+    productStatus: 0,
+    categoryType: 0,
+    createTime: '2018-03-10T11:44:25.000+0000',
+    updateTime: '2018-03-10T11:44:25.000+0000'
+  },
+  {
+    productId: 'C0002',
+    productName: 'iPhone XS',
+    productPrice: 20.00,
+    productStock: 195,
+    productDescription: 'Apple iPhone',
+    productIcon: 'https://images-na.ssl-images-amazon.com/images/I/51gHy16h5TL._SX397_BO1,204,203,200_.jpg',
+    productStatus: 0,
+    categoryType: 0,
+    createTime: '2018-03-10T15:35:43.000+0000',
+    updateTime: '2018-03-10T15:35:43.000+0000'
+  },
+  {
+    productId: 'C0001',
+    productName: 'Samsung Galaxy',
+    productPrice: 30.00,
+    productStock: 96,
+    productDescription: 'Samsung',
+    productIcon: 'https://images-na.ssl-images-amazon.com/images/I/41f6Rd6ZEPL._SX363_BO1,204,203,200_.jpg',
+    productStatus: 1,
+    categoryType: 0,
+    createTime: '2018-03-10T11:44:25.000+0000',
+    updateTime: '2018-03-10T11:44:25.000+0000'
+  },
+  {
+    productId: 'C0002',
+    productName: 'Sony Camera',
+    productPrice: 20.00,
+    productStock: 195,
+    productDescription: 'Sony HD',
+    productIcon: 'https://images-na.ssl-images-amazon.com/images/I/51gHy16h5TL._SX397_BO1,204,203,200_.jpg',
+    productStatus: 0,
+    categoryType: 0,
+    createTime: '2018-03-10T15:35:43.000+0000',
+    updateTime: '2018-03-10T15:35:43.000+0000'
+  }, {
+    productId: 'C0001',
+    productName: 'Apple Tab',
+    productPrice: 30.00,
+    productStock: 96,
+    productDescription: 'Apple tablet',
+    productIcon: 'https://images-na.ssl-images-amazon.com/images/I/41f6Rd6ZEPL._SX363_BO1,204,203,200_.jpg',
+    productStatus: 1,
+    categoryType: 0,
+    createTime: '2018-03-10T11:44:25.000+0000',
+    updateTime: '2018-03-10T11:44:25.000+0000'
+  },
+  {
+    productId: 'C0002',
+    productName: 'iPhone 13 Pro MAX',
+    productPrice: 20.00,
+    productStock: 195,
+    productDescription: 'Apple iPhone',
+    productIcon: 'https://images-na.ssl-images-amazon.com/images/I/51gHy16h5TL._SX397_BO1,204,203,200_.jpg',
+    productStatus: 0,
+    categoryType: 0,
+    createTime: '2018-03-10T15:35:43.000+0000',
+    updateTime: '2018-03-10T15:35:43.000+0000'
+  }];
+
+export const prod: ProductInfo = {
+  productId: 'C0002',
+  productName: 'Macbook Pro',
+  productPrice: 20.00,
+  productStock: 195,
+  productDescription: 'Apple Macbook',
+  productIcon: 'https://images-na.ssl-images-amazon.com/images/I/51gHy16h5TL._SX397_BO1,204,203,200_.jpg',
+  productStatus: 0,
+  categoryType: 0,
+  createTime: '2018-03-10T15:35:43.000+0000',
+  updateTime: '2018-03-10T15:35:43.000+0000'
+};
+
